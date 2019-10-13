8955aabb8e10fb119e3ff6e7c60447d7ceec7d1a
Praveen Upadhyay <praveenupadhyay20@gmail.com>
2019-10-13 10:02:46 +0530

    Survey API

---
+++ SurveyAPI/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using SurveyAPI.DTOS;
+using SurveyAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SurveyAPI.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+        }
+    }
+}
